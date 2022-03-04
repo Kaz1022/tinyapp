4e6886ed3d51ac5ed17b39da763de3395b5332a1
Kazuko Baynton <kazukobaynton@gmail.com>
2022-03-03 16:07:19 -0800

    install body-parser and create generateRandomString functino on express_server.js

--- express_server.js
+++ express_server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+  const randomString = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2,5);
+  return randomString;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,11 +33,19 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 
 app.listen(PORT, () => {
