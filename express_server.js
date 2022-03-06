3ea8cb28c5606853868bfc547ebe130200e7894f
Kazuko Baynton <kazukobaynton@gmail.com>
2022-03-05 20:22:51 -0800

    feature/cookies add cookie function and implement login and logout bar

--- express_server.js
+++ express_server.js
@@ -1,39 +1,40 @@
 const express = require('express');
-const app = express();
-const PORT = 8080; //default port 8080
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+const cookieParser = require('cookie-parser');
+
+const PORT = 8080; //default port 8080
+const app = express();
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// generate random alphanumeric string with 6 characters, shortURL
 function generateRandomString() {
   const randomString = Math.random().toString(36).slice(2, 8);
   return randomString;
 };
 
+// base database for URL
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  // "b2xVn2": "http://www.lighthouselabs.ca",
+  // "9sm5xK": "http://www.google.com"
 };
-
-app.get('/', (req, res) => {
-  res.send("Hello!");
-});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
+// rendering shortURLs and longURLs & username cookie to urls_index
 app.get('/urls', (req, res) => {
-  console.log("app get urls")
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies.username
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -42,20 +43,26 @@ app.get('/urls', (req, res) => {
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
-  
   urlDatabase[shortURL] = longURL;
-
   res.redirect(`/urls/${shortURL}`);
 });
 
+// rendering to url_new username cookie 
 app.get('/urls/new', (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies.username
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   console.log(urlDatabase[shortURL]);
-  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  const templateVars = { 
+    shortURL: shortURL, 
+    longURL: urlDatabase[shortURL],
+    username: req.cookies.username 
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -80,4 +87,17 @@ app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+// create an endpoint to handle a POST to /login
+// also create cookie with username 🍪
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+// logout/clear cookie
+app.post('/logout', (req,res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
 
